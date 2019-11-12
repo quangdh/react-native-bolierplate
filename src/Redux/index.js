d4e9a68d784bf08082996ed4e8968ca0ea418e73
Huy Quang Do <>
2019-11-12 15:04:13 +0700

    Remove search redux

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,14 +1,13 @@
-import { combineReducers } from "redux";
-import { persistReducer } from "redux-persist";
-import configureStore from "./CreateStore";
-import rootSaga from "../Sagas/";
-import ReduxPersist from "../Config/ReduxPersist";
+import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
+import configureStore from './CreateStore';
+import rootSaga from '../Sagas/';
+import ReduxPersist from '../Config/ReduxPersist';
 
 /* ------------- Assemble The Reducers ------------- */
 export const reducers = combineReducers({
-  nav: require("./NavigationRedux").reducer,
-  github: require("./GithubRedux").reducer,
-  search: require("./SearchRedux").reducer
+  nav: require('./NavigationRedux').reducer,
+  github: require('./GithubRedux').reducer,
 });
 
 export default () => {
@@ -19,17 +18,17 @@ export default () => {
     finalReducers = persistReducer(persistConfig, reducers);
   }
 
-  let { store, sagasManager, sagaMiddleware } = configureStore(
+  let {store, sagasManager, sagaMiddleware} = configureStore(
     finalReducers,
-    rootSaga
+    rootSaga,
   );
 
   if (module.hot) {
     module.hot.accept(() => {
-      const nextRootReducer = require("./").reducers;
+      const nextRootReducer = require('./').reducers;
       store.replaceReducer(nextRootReducer);
 
-      const newYieldedSagas = require("../Sagas").default;
+      const newYieldedSagas = require('../Sagas').default;
       sagasManager.cancel();
       sagasManager.done.then(() => {
         sagasManager = sagaMiddleware(newYieldedSagas);
